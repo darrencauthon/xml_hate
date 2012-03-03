0ecacc6c574de4c92ab429149d2c4ef256c0860d
Darren Cauthon <darren@cauthon.com>
2012-03-03 09:43:44 -0600

    Fix the previous simple tests.

--- lib/xml_hate/document.rb
+++ lib/xml_hate/document.rb
@@ -7,9 +7,16 @@ module XmlHate
     end
 
     def method_missing(meth, *args, &blk)
-      return_value = Hashie::Mash.new
-      return_value[meth.to_s] = Hashie::Mash.new(@document[meth.to_s][0])
-      return return_value[meth.to_s]
+      begin 
+        return_value = Hashie::Mash.new
+        if @document[meth.to_s][0].class == String
+          return @document[meth.to_s][0]
+        end
+        return_value[meth.to_s] = Hashie::Mash.new(@document[meth.to_s][0])
+        return return_value[meth.to_s]
+      rescue 
+        ""
+      end
     end
   end
 end
