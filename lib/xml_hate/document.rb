e5e5652e5846e801b002ce1ff2312fde1d0682df
Darren Cauthon <darren@cauthon.com>
2012-03-04 14:26:19 -0600

    Continue to shrink the code.

--- lib/xml_hate/document.rb
+++ lib/xml_hate/document.rb
@@ -23,18 +23,21 @@ module XmlHate
 
     def push_single_elements_up_to_attributes(node)
       values_to_pluralize = {}
-      node.each do |key, value|
-        begin
-          if value.count >= 1
-            values_to_pluralize[key.pluralize] = value if key.pluralize != key
-            cleaned_values = value.map { |v| push_single_elements_up_to_attributes(v) }
-          end
-          node[key] = (value.count == 1 ? value[0] : value)
-        rescue
-        end
+      node.select{|k,v| get_the_number_of_elements(v) >= 1}.each do |key, value|
+        values_to_pluralize[key.pluralize] = value if key.pluralize != key
+        cleaned_values = value.map { |v| push_single_elements_up_to_attributes(v) }
+        node[key] = get_the_number_of_elements(value) == 1 ? value[0] : value
       end
       values_to_pluralize.each { |key, value| node[key] = value }
       node
+    end
+
+    def get_the_number_of_elements(value)
+      begin
+        value.count
+      rescue
+        0
+      end
     end
   end
 end
