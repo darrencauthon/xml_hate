8171f3ec7ba1a546e345b71cfd13e7dccea963cf
Darren Cauthon <darren@cauthon.com>
2012-03-03 21:06:45 -0600

    Refactor.

--- lib/xml_hate/document.rb
+++ lib/xml_hate/document.rb
@@ -6,32 +6,33 @@ module XmlHate
       @document = XmlSimple.xml_in(xml)
     end
 
-    def push_single_elements_up_to_attributes(value)
-        value.each do |k, v|
-          begin
-            value[k] = v[0] if v.count == 1 
-            if v.count > 1
-              v.each do |i|
-                push_single_elements_up_to_attributes i
-              end
-            end
-          rescue
-          end
-        end
-    end
-    
     def method_missing(meth, *args, &blk)
       begin
-        return_value = Hashie::Mash.new
-
         this_node = @document[meth.to_s][0]
         return this_node if this_node.class == String
 
+        return_value = Hashie::Mash.new
         return_value[meth.to_s] = Hashie::Mash.new(this_node)
 
-       push_single_elements_up_to_attributes(return_value[meth.to_s])
+        push_single_elements_up_to_attributes(return_value[meth.to_s])
         return return_value[meth.to_s]
       rescue
+      end
+    end
+
+    private
+
+    def push_single_elements_up_to_attributes(node)
+      node.each do |key, value|
+        begin
+          node[key] = value[0] if value.count == 1 
+          if value.count > 1
+            value.each do |i|
+              push_single_elements_up_to_attributes i
+            end
+          end
+        rescue
+        end
       end
     end
   end
