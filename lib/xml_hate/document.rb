7b431cf0849ffa7b59a3e018845fc895d5688175
Darren Cauthon <darren@cauthon.com>
2012-03-04 14:12:20 -0600

    Remove another rescue.

--- lib/xml_hate/document.rb
+++ lib/xml_hate/document.rb
@@ -10,18 +10,15 @@ module XmlHate
     def method_missing(meth, *args, &blk)
       return "" if @document.has_key?(meth.to_s) == false 
       return_values = @document[meth.to_s].map { |n| process_this_node(n) }
-      return_values.count ==1 ? return_values[0] : return_values
+      return_values.count == 1 ? return_values[0] : return_values
     end
 
     private
 
     def process_this_node(node)
-      begin
-        new_object = Hashie::Mash.new(node)
-        push_single_elements_up_to_attributes(new_object)
-      rescue
-        ""
-      end
+      return node if node.class == String
+      new_object = Hashie::Mash.new(node)
+      push_single_elements_up_to_attributes(new_object)
     end
 
     def push_single_elements_up_to_attributes(node)
