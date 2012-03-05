f7c2c58bd44eac8ee945315843cde2be08276560
Darren Cauthon <darren@cauthon.com>
2012-03-04 21:12:17 -0600

    More refactoring so the code could be understood.

--- lib/xml_hate/node.rb
+++ lib/xml_hate/node.rb
@@ -1,16 +1,18 @@
 module XmlHate
   class Node
     def initialize(hash)
-      hash.each do |k,v|
-        create_accessor_for k, clean_up_the_value(v) 
-      end
+      hash.each { |k,v| create_accessor_for k, convert_the_value_to_the_appropriate_form(v) }
     end
 
     def method_missing(meth, *args, &blk)
-      meth.to_s == meth.to_s.pluralize ? [] : ""
+      an_empty_array_for_plurals_or_empty_string_for_singulars meth
     end
 
     private
+
+    def an_empty_array_for_plurals_or_empty_string_for_singulars(meth)
+      meth.to_s == meth.to_s.pluralize ? [] : ""
+    end
 
     def create_accessor_for(k, v)
       self.instance_variable_set("@#{k}", v)
@@ -18,7 +20,7 @@ module XmlHate
       self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)}) 
     end
 
-    def clean_up_the_value(value)
+    def convert_the_value_to_the_appropriate_form(value)
       return Node.new(value) if value.class == Hashie::Mash 
       return value.map {|i| i.class == Hashie::Mash ? Node.new(i) : i} if value.class == Array
       value
