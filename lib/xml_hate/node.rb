b996d685092abfaecc4322e09360c5bb23e513ce
Darren Cauthon <darren@cauthon.com>
2012-03-04 21:05:14 -0600

    Cleanup.

--- lib/xml_hate/node.rb
+++ lib/xml_hate/node.rb
@@ -2,14 +2,7 @@ module XmlHate
   class Node
     def initialize(hash)
       hash.each do |k,v|
-        v = Node.new(v) if v.class == Hashie::Mash 
-        if v.class == Array
-          new_values = []
-          v.each do |i|
-            new_values << (i.class == Hashie::Mash ? Node.new(i) : i)
-          end
-          v = new_values
-        end
+        v = clean_up_the_value(v)
         self.instance_variable_set("@#{k}", v)
         self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
         self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)}) 
@@ -18,6 +11,20 @@ module XmlHate
 
     def method_missing(meth, *args, &blk)
       meth.to_s == meth.to_s.pluralize ? [] : ""
+    end
+
+    private
+
+    def clean_up_the_value(value)
+      return Node.new(value) if value.class == Hashie::Mash 
+      if value.class == Array
+        new_values = []
+        value.each do |i|
+          new_values << (i.class == Hashie::Mash ? Node.new(i) : i)
+        end
+        return new_values
+      end
+      value
     end
   end
 end
