ccb2c920aea95ef72ae17aadcdb545cb56f3a75e
Darren Cauthon <darren@cauthon.com>
2012-03-04 21:09:20 -0600

    More cleanup.

--- lib/xml_hate/node.rb
+++ lib/xml_hate/node.rb
@@ -2,10 +2,7 @@ module XmlHate
   class Node
     def initialize(hash)
       hash.each do |k,v|
-        v = clean_up_the_value(v)
-        self.instance_variable_set("@#{k}", v)
-        self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
-        self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)}) 
+        create_accessor_for k, clean_up_the_value(v) 
       end
     end
 
@@ -14,6 +11,12 @@ module XmlHate
     end
 
     private
+
+    def create_accessor_for(k, v)
+      self.instance_variable_set("@#{k}", v)
+      self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
+      self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)}) 
+    end
 
     def clean_up_the_value(value)
       return Node.new(value) if value.class == Hashie::Mash 
